93cda77574154657914db050751569461dbb15fd
sivaedara <sivaedara1096@gmail.com>
2022-01-18 11:49:12 -0800

    New python script to start/stop vagrant boxes

---
+++ startVagrantBoxes.py
@@ -0,0 +1,25 @@
+""" This will spin up/start up local linux VM's """
+
+import os 
+
+foldersList=["BuildServer","devapp1","devapp2"]
+
+
+def startVagrantBoxes():
+    """ This will start vagrant boxes using the ip's listed listofIps """
+    
+    cwd= os.getcwd()
+    for folder in foldersList:
+        os.chdir(folder)
+        print ("\n\nstarting vagrant box from "+folder)
+        os.system("vagrant up")
+        # to stop vagrant boxes update command with "vagrant halt"
+        # to initiate vagrant boxes use command "vagrant init"
+        os.chdir(cwd)
+
+def main():
+    """ Main function to call python script """
+
+    startVagrantBoxes()
+
+main()
